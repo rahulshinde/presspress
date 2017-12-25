8062f7d5e43149da455588052e4fd31288f4e029
Rahul Shinde <rshinde@mica.edu>
2017-12-25 14:23:01 -0500

    rd

---
+++ js/main.js
@@ -0,0 +1,11 @@
+Site = {}
+
+$(document).ready( function(){
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true,
+    freeScroll: true
+  });
+})
