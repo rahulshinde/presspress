564c596654803edc73349202d31f541b1d2e99ee
Rahul Shinde <rshinde@mica.edu>
2017-12-25 22:17:44 -0500

    adding new images + format

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 Site = {}
 
-$(document).ready( function(){
-  $('.main-carousel').flickity({
-    // options
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-    freeScroll: true
-  });
+$(document).ready(function(){
+  Site.document_height = $(document).height() - $(window).height();
+  $(document).on('scroll', scrollImages);
+
+  $(window).on("load", function() {
+    Site.image_height = $('.images').height() - $(window).height();
+  })
 })
+
+scrollImages = function(){
+  scroll_pos = $(document).scrollTop();
+  proportion = scroll_pos/Site.document_height;
+
+  $('.images').css('transform', 'translateY(-' + proportion * Site.image_height + 'px)');
+}
